5cf855f098ceb7edbd18add7177d40ef6bcf980f
Ramzy Karam <Ramzy.karam92@gmail.com>
2015-12-24 21:13:29 +0200

    save gulpfile just in case

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,25 @@ var gulp = require('gulp');
 gulp.task('default', function(){
   console.log('Hello From Gulp');
 });
+
+
+var responsive = require('gulp-responsive');
+
+gulp.task('images', function () {
+  return gulp.src('raw/*.jpg')
+    .pipe(responsive([{
+      name: '*.jpg',
+      width: 200
+    },{
+      name: '*.jpg',
+      width: 200 * 2,
+      rename: '*@2x.jpg'
+    },{
+      name: 'background-*.jpg',
+      width: 700
+    },{
+      name: '*.jpg',
+      width: '50%'
+    }]))
+    .pipe(gulp.dest('images'));
+});
